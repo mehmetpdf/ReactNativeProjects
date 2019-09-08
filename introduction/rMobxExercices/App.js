23da5d0486becf6788c4b315c2adc10692ee6866
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-08 16:04:57 +0300

    install mobx and setting babelrc

--- introduction/rMobxExercices/App.js
+++ introduction/rMobxExercices/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
@@ -11,3 +12,17 @@ export default class App extends Component<Props> {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
