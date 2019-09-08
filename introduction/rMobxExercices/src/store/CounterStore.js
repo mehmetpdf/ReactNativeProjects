87a64bb8928f14de57ff4fc1481d18233b33c464
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-08 19:26:06 +0300

    "autorun" examples

--- introduction/rMobxExercices/src/store/CounterStore.js
+++ introduction/rMobxExercices/src/store/CounterStore.js
@@ -1,7 +1,18 @@
-import {action, observable} from "mobx";
+import {action, autorun, observable} from "mobx";
 
 class CounterStore {
     @observable count = 1 // observable demek count degiskenini uygulamanin her yerinden takip edebilecegiz..
+
+
+    /**
+     * autorun initial durumda ve change durumunda direk calisir.
+     */
+    constructor(){
+        autorun(() => {
+            alert(this.count)
+        })
+    };
+
 
     @action azalt = () => {
         this.count--;
