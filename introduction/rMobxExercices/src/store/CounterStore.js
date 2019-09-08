6d020053ab892738410589bebad650a9492aa68f
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-08 19:36:04 +0300

    "when" examples

--- introduction/rMobxExercices/src/store/CounterStore.js
+++ introduction/rMobxExercices/src/store/CounterStore.js
@@ -1,4 +1,4 @@
-import {action, autorun, observable ,reaction} from "mobx";
+import {action, autorun, observable ,reaction, when} from "mobx";
 
 class CounterStore {
     @observable count = 1 // observable demek count degiskenini uygulamanin her yerinden takip edebilecegiz..
@@ -8,20 +8,32 @@ class CounterStore {
      * autorun initial durumda ve change durumunda direk calisir.
      */
     constructor(){
-        // autorun(() => {
-        //     alert(this.count)
-        // });
+        /*autorun(() => {
+            alert(this.count)
+        });*/
 
         /**
          * Sorun su: count 5 olunca basardi yazsin ekrana. Bu durumda azalt() ve arttir()
          * fonksiyonlarinin icine iki defa if yazmak zorunda kalacaktik.
          * ancak biz bu durumda reaction ile kurtulmus oluyoruz..
          */
-        reaction(() => {this.count},
+        /*reaction(() => {this.count},
             (count) => {
             if(count === 5)
                 alert("Başardın");
-            })
+            });*/
+
+
+        /**
+         * Sorun su: count 5 olunca basardi yazsin ekrana. Bu durumda azalt() ve arttir()
+         * fonksiyonlarinin icine iki defa if yazmak zorunda kalacaktik.
+         * ancak when ile boolean bir deger olup olmadigini ilk parametrede belirliyoruz.
+         * eger true olursa opts degiskeni bir kereye mahsus calisiyor.
+         */
+        when(() => this.count === 5,
+            () => {alert("Başardın")})
+
+
     };
 
 
