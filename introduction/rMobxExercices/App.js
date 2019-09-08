8ef8683c68b5c2264f8d80d423ce0d05577bd49d
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-08 15:36:14 +0300

    initial commits for mobx

---
+++ introduction/rMobxExercices/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
