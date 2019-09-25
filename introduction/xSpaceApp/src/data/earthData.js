5d3d690ffc56e7a7ac7a0f41bb7ade46ea6d1a93
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-25 23:44:40 +0300

    earth data olusturuldu ve listelendi

---
+++ introduction/xSpaceApp/src/data/earthData.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        "_id": "e1",
+        "picture": "https://picsum.photos/50",
+        "question": "What Is a Tsunami?",
+    },
+    {
+        "_id": "e2",
+        "picture": "https://picsum.photos/50",
+        "question": "Supermoon, Blood Moon, Blue Moon and Harvest Moon",
+    },
+    {
+        "_id": "e3",
+        "picture": "https://picsum.photos/50",
+        "question": "What Is an Earthquake?",
+    },
+
+];
--- introduction/xSpaceApp/src/screens/EarthScreen.js
+++ introduction/xSpaceApp/src/screens/EarthScreen.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList, TouchableOpacity, SafeAreaView, Image } from 'react-native';
+import earthData from "../data/earthData";
+import TouchableScale from 'react-native-touchable-scale'; // https://github.com/kohver/react-native-touchable-scale
+import LinearGradient from 'react-native-linear-gradient'; // Only if no expo
+import { ListItem } from 'react-native-elements'
+
 
 export default class EarthScreen extends Component {
+
+    state = {
+        questions: earthData,
+
+    };
+
+    _renderQuestionsItem = ({item, index}) => {
+        return(
+            /*<TouchableOpacity>
+                <Image
+                    style={styles.avatar}
+                    source={{uri: item.picture}}
+                />
+                <View>
+                    <Text>{item.question}</Text>
+                </View>
+            </TouchableOpacity>*/
+            <ListItem
+                Component={TouchableScale}
+                friction={90} //
+                tension={100} // These props are passed to the parent component (here TouchableScale)
+                activeScale={0.95} //
+                linearGradientProps={{
+                    colors: ['#FF9800', '#F44336'],
+                }}
+                ViewComponent={LinearGradient} // Only if no expo
+                leftAvatar={{ rounded: true, source: {uri: item.picture} }}
+                title={item.question}
+                titleStyle={{ color: 'white', fontWeight: 'bold' }}
+                subtitleStyle={{ color: 'white' }}
+                subtitle={item.question}
+                chevron={{ color: 'white' }}
+                style={styles._item}
+            />
+
+        )
+    };
+
     render() {
         return (
-            <View>
-                <Text>
-                    EarthScreen
-                </Text>
-            </View>
+            <SafeAreaView>
+                <FlatList
+                    renderItem={this._renderQuestionsItem}
+                    keyExtractor={(item) => item._id}
+                    data={this.state.questions}
+                />
+            </SafeAreaView>
         );
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    avatar: {
+        width: 50,
+        height: 50
+    },
+    _item: {
+        margin: 5
+    }
+});
